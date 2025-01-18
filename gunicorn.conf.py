afea3e091d012cc7b786ef54de012ab3cbf7635c
vaishTechie <vaishuagrawal111@gmail.com>
2025-01-18 15:56:10 +0530

    Add files via upload

---
+++ gunicorn.conf.py
@@ -0,0 +1,18 @@
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:$PORT"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+errorlog = '-'
+loglevel = 'info'
