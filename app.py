bff8d2ca5958433c45f5c13a0dcd60843dc7f33c
vaishTechie <vaishuagrawal111@gmail.com>
2025-01-18 16:52:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,5 +230,5 @@ def health_check():
 import os
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    port = int(os.getenv("PORT", 8080))  # Default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
